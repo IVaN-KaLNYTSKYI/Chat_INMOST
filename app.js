0e2c2faed1dc43cb6df8264b533f48d93a817aa2
IVaN-KaLNYTSKYI <68811887+IVaN-KaLNYTSKYI@users.noreply.github.com>
2022-04-13 16:19:06 +0300

    er

--- app.js
+++ app.js
@@ -8,24 +8,73 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
 	res.render('index')
 })
+app.get('/group', (req, res) => {
+    res.render('index-2')
+})
 server = app.listen("3000", () => console.log("Server is running..."));
 
 const io = require("socket.io")(server);
 
+let users = []
+
 io.on('connection', (socket) => {
-	console.log('New user connected')
+    console.log(`CONNECTION ${socket.id}`);
 
-	socket.username = "Anonymous"
+    socket.on('user_connect',(data)=>{
+        console.log(data,`CON ${socket.id}`);
+        users[data.id]=socket.id;
+        console.log(users,'USER LIST');
+        io.sockets.emit("user_list", {
+            list:users
+        });
+    })
+    socket.on("join_room", (data) => {
+        console.log(users);
+        socket.join(data.room);
+    });
 
-    socket.on('change_username', (data) => {
-        socket.username = data.username
+    socket.on("create_message_server", (data) => {
+        console.log(socket.rooms);
+        socket.broadcast.to(data.room).emit("create_message_client", {
+            message:data.message,
+            name: socket.id
+        });
+    });
+
+    socket.on("create_private_message_server", (data) => {
+        console.log(data,'create_private_message_server');
+        io.sockets.to(users[data.userId]).emit("create_private_message_client",{
+            message:data.message,
+            name: socket.id
+        })
+    });
+
+    socket.on("add_user_group", (data) => {
+        socket.emit("add_user_group_client", {
+            message:`Welcome to the group ${data.room}`
+        });
+        console.log(socket.rooms);
+        socket.broadcast.to(data.room).emit("add_user_group_client_2", {
+            message:`Joined the chat ${socket.id}`
+        });
+    });
+
+    socket.on("remove_user_room)", (data) => {
+        socket.leave(data.room);
+    });
+
+    socket.on('typing_server', (data) => {
+        socket.broadcast.to(data.room).emit('typing_client', {username :socket.id })
     })
 
-    socket.on('new_message', (data) => {
-        io.sockets.emit('add_mess', {message : data.message, username : socket.username, className:data.className});
+    socket.on('typing_privat_server', (data) => {
+        io.sockets.to(users[data.userId]).emit('typing_privat_client', {username :socket.id })
     })
 
-    socket.on('typing', (data) => {
-    	socket.broadcast.emit('typing', {username : socket.username})
-    })
+    socket.on("disconnect", () => {
+        console.log(socket.id);
+        let userIndex = users.indexOf(socket.id);
+        users.splice(userIndex,userIndex);
+        console.log(users,'USER WHEN DISCONNECT');
+    });
 })
--- public/chat.js
+++ public/chat.js
@@ -1,72 +1,116 @@
-$(function() {
-  var socket = io.connect("http://localhost:3000");
+$(function () {
+    var socket = io.connect("http://localhost:3000");
+    var message = $("#message");
+    var send_message = $("#send_message");
+    var chatroom = $("#chatroom");
+    var feedback = $("#feedback");
+    var send_login = $("#send_login");
+    var login = $("#login");
+    var change_list_username = $("#change_list_username");
+    var message_privat = $("#message_privat");
+    var send_message_privat = $("#send_message_privat");
+    var nameUser = $("#nameUser");
+    var send_join_room = $("#send_join_room");
+    var join_room = $("#join_room");
+    var user_write = $("#user_write");
+    var name = ''
+    var arr=[]
 
-  var message = $("#message");
-  var username = $("#username");
-  var send_message = $("#send_message");
-  var send_username = $("#send_username");
-  var chatroom = $("#chatroom");
-  var feedback = $("#feedback");
-
-  send_message.click(() => {
-    socket.emit("new_message", {
-      message: message.val(),
-      className: alertClass
+    socket.on("user_list", (data) => {
+        change_list_username.html("")
+        $('#userList').html("")
+        console.log(data);
+        $.each(data.list, function (index, value) {
+            console.log(value);
+            $('#change_list_username')
+                .append($("<option></option>")
+                    .attr("value", index)
+                    .text(value));
+        });
+        arr=[...data.list]
     });
-  });
-  var min = 1;
-  var max = 6;
-  var random = Math.floor(Math.random() * (max - min)) + min;
+    send_login.click(() => {
+        socket.emit("user_connect", {
+            id: login.val()
+        });
+        $('#input_zon').html('')
+    });
 
-  // Устаналиваем класс в переменную в зависимости от случайного числа
-  // Эти классы взяты из Bootstrap стилей
-  var alertClass;
-  switch (random) {
-    case 1:
-      alertClass = "secondary";
-      break;
-    case 2:
-      alertClass = "danger";
-      break;
-    case 3:
-      alertClass = "success";
-      break;
-    case 4:
-      alertClass = "warning";
-      break;
-    case 5:
-      alertClass = "info";
-      break;
-    case 6:
-      alertClass = "light";
-      break;
-  }
+    user_write.click(() => {
+        console.log(change_list_username.val());
+        nameUser.html('')
+        if(arr[change_list_username.val()]===socket.id){
+            alert('Error')
+        }
+        else {
+            nameUser.append($("<div></div>").text(`We write to ${arr[change_list_username.val()]}`))
+            name = change_list_username.val()
+        }
 
-  socket.on("add_mess", data => {
-    feedback.html("");
-    message.val("");
-    chatroom.append(
-      "<div class='alert alert-" +
-        data.className +
-        "'<b>" +
-        data.username +
-        "</b>: " +
-        data.message +
-        "</div>"
-    );
-  });
+    });
 
-  send_username.click(() => {
-    socket.emit("change_username", { username: username.val() });
-  });
+    send_message_privat.click(() => {
+        socket.emit("create_private_message_server", {
+            userId: name,
+            message:message_privat.val(),
+        });
+        message_privat.val(' ')
+    });
 
-  message.bind("keypress", () => {
-    socket.emit("typing");
-  });
+/*
+    change_list_username.on('change', function (e) {
+        nameUser.html('')
+        nameUser.append($("<div></div>").text(`We write to ${arr[e.target.value]}`))
+        name = e.target.value
+    });
+*/
 
-  socket.on("typing", data => {
-    feedback.html(
-      "<p><i>" + data.username + " печатает сообщение..." + "</i></p>"
-    );
-  });
+    socket.on("create_private_message_client", data => {
+        feedback.html("");
+        chatroom.append($("<div></div>").text(`${data.name}:${data.message}`))
+    });
+
+
+    message_privat.bind("keypress", () => {
+        socket.emit("typing_privat_server",{
+            userId:name
+        });
+    });
+
+    socket.on("typing_privat_client", data => {
+        feedback.html(
+            "<p><i>" + data.username + " prints..." + "</i></p>"
+        );
+    });
+
+    //  GROUP CHAT INMOST
+    send_join_room.click(()=>{
+        socket.emit("join_room", {
+            room: 'Inmost'
+        });
+    })
+
+    send_message.click(() => {
+        socket.emit("create_message_server", {
+            room:'Inmost',
+            message: message.val()
+        });
+        message.val(' ')
+    });
+    socket.on("create_message_client", data => {
+        feedback.html("");
+        chatroom.append($("<div></div>").text(`${data.name}:${data.message}`))
+    });
+
+    message.bind("keypress", () => {
+        socket.emit("typing_server",{
+            room:'Inmost'
+        });
+    });
+
+    socket.on("typing_client", data => {
+        feedback.html(
+            "<p><i>" + data.username + " prints..." + "</i></p>"
+        );
+    });
 });
